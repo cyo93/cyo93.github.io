ecdbc0c5f8dfdb191d65b45ed8c09ba532cb43f9
cyo93 <59427615+cyo93@users.noreply.github.com>
2025-12-02 16:41:22 -0800

    Current page is highlighted on menu bar.

--- menu.js
+++ menu.js
@@ -1,6 +1,7 @@
-document.getElementById("menu").innerHTML = '<div class="topmenu"><div class="mobilehome"><a href="index.html">Home</a></div><ul id="menusdesktop"><li id="menudesktopitem"><a href="index.html">Home</a></li><li id="menudesktopitem"><a href="about.html">About Me</a></li><li id="menudesktopitem"><a href="Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li><li id="menudesktopitem"><a href="projects.html">Personal Projects</a></li><li id="menudesktopitem"><a href="articles.html">Thoughts</a></li><li id="menudesktopitem"><a href="contact.html">Contact</a></li></ul><a href="javascript:void(0);" class="mobilemenu" onclick="showMobileMenu()"><i class="fa fa-bars"></i></a><div class="menus" id="menus"><ul id="mobilemenus"><li><a href="about.html">About Me</a></li><li><a href="Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li><li><a href="projects.html">Personal Projects</a></li><li><a href="articles.html">Thoughts</a></li><li><a href="contact.html">Contact</a></li></div></div>';
+document.getElementById("menu").innerHTML = '<div class="topmenu"><div class="mobilehome"><a href="index.html">Home</a></div><ul id="menusdesktop"><li class="menudesktopitem"><a href="index.html">Home</a></li><li class="menudesktopitem"><a href="about.html">About Me</a></li><li class="menudesktopitem"><a href="Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li><li class="menudesktopitem"><a href="projects.html">Personal Projects</a></li><li class="menudesktopitem"><a href="articles.html">Thoughts</a></li><li class="menudesktopitem"><a href="contact.html">Contact</a></li></ul><a href="javascript:void(0);" class="mobilemenu" onclick="showMobileMenu()"><i class="fa fa-bars"></i></a><div class="menus" id="menus"><ul id="mobilemenus"><li class="mobilemenuitem"><a href="about.html">About Me</a></li><li class="mobilemenuitem"><a href="Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li><li class="mobilemenuitem"><a href="projects.html">Personal Projects</a></li><li class="mobilemenuitem"><a href="articles.html">Thoughts</a></li><li class="mobilemenuitem"><a href="contact.html">Contact</a></li></div></div>';
 menus = document.getElementById("menus");
 menus.style.display = "none";
+getcurrentmenu();
 function showMobileMenu(){
 	menus = document.getElementById("menus");
 	if(menus.style.display == "none"){
@@ -10,4 +11,29 @@ function showMobileMenu(){
 		menus.style.display = "none";
 	}
 	
+}
+
+function getcurrentmenu(){
+	list = document.getElementsByClassName("menudesktopitem");
+	mobilelist = document.getElementsByClassName("mobilemenuitem");
+	if(document.title.includes("Homepage")){
+		list[0].setAttribute("highlighted", "true");
+		document.getElementsByClassName("mobilehome")[0].setAttribute("highlighted", "true");
+	}
+	if(document.title.includes("About")){
+		list[1].setAttribute("highlighted", "true");
+		mobilelist[0].setAttribute("highlighted", "true");
+	}
+	if(document.title.includes("Projects")){
+		list[3].setAttribute("highlighted", "true");
+		mobilelist[2].setAttribute("highlighted", "true");
+	}
+	if(document.title.includes("Thoughts")){
+		list[4].setAttribute("highlighted", "true");
+		mobilelist[3].setAttribute("highlighted", "true");
+	}
+	if(document.title.includes("Contact")){
+		list[5].setAttribute("highlighted", "true");
+		mobilelist[4].setAttribute("highlighted", "true");
+	}
 }
